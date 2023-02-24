11012db476823384a9103aac45b41990739f67bd
Jace Key <97260514+jacekey-snyk@users.noreply.github.com>
2023-02-24 00:06:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 SECRET_KEY = 12
 print(SECRET_KEY)
 print("hi")
+import psycopg2
+
+connection = psycopg2.connect(
+    host="localhost",
+    database="psycopgtest",
+    user="postgres",
+    password=None,
+)
+connection.set_session(autocommit=True)
